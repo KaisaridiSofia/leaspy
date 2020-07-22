corrected numerical issue which could lead to unwanted NaN in specific cases

leaspy/models/abstract_model.py
@@ -318,7 +318,7 @@ class AbstractModel:
             # noise_var = self.parameters['noise_std'] ** 2
             noise_var = self.parameters['noise_std'] * self.parameters['noise_std']
             attachment = 0.5 * (1. / noise_var) * squared_sum
-            attachment += math.log(math.sqrt(TWO_PI * noise_var)) * torch.tensor(data.nb_observations_per_individuals)
+            attachment += torch.log(torch.sqrt(TWO_PI * noise_var)) * torch.tensor(data.nb_observations_per_individuals)
         elif self.loss == 'crossentropy':
             res = torch.clamp(res, 1e-38, 1. - 1e-7) # safety before taking the log
             neg_crossentropy = data.values * torch.log(res) + (1. - data.values) * torch.log(1. - res)

leaspy/models/univariate_model.py
@@ -187,12 +187,12 @@ class UnivariateModel(AbstractModel):
 
         tau_mean = self.parameters['tau_mean']
         tau_std_updt = torch.mean(suff_stats['tau_sqrd']) - 2 * tau_mean * torch.mean(suff_stats['tau'])
-        self.parameters['tau_std'] = torch.sqrt(tau_std_updt + self.parameters['tau_mean'] ** 2)
+        self.parameters['tau_std'] = torch.sqrt(torch.clamp(tau_std_updt + self.parameters['tau_mean'] ** 2, 0.))
         self.parameters['tau_mean'] = torch.mean(suff_stats['tau'])
 
         xi_mean = self.parameters['xi_mean']
         xi_std_updt = torch.mean(suff_stats['xi_sqrd']) - 2 * xi_mean * torch.mean(suff_stats['xi'])
-        self.parameters['xi_std'] = torch.sqrt(xi_std_updt + self.parameters['xi_mean'] ** 2)
+        self.parameters['xi_std'] = torch.sqrt(torch.clamp(xi_std_updt + self.parameters['xi_mean'] ** 2, 0.))
         self.parameters['xi_mean'] = torch.mean(suff_stats['xi'])
 
         S1 = torch.sum(suff_stats['obs_x_obs'])